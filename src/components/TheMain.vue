2b40af249f90dfa5b80165413ec14bf328b2fc22
YevhenKataiev <yevhen.kataiev@sigma.software>
2023-08-15 17:46:29 +0300

    Initial commit

---
+++ src/components/TheMain.vue
@@ -0,0 +1,18 @@
+<template>
+  {{breed}}
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+const breed = ref({});
+onMounted(async() => {
+  const config = {
+    headers: { 'x-api-key' : 'live_4A66sBHfE3ydx7n3QVnwlRVK4wQIgzzp30fRsxRLBl3aKNLgdEyUHigpRcjfprB9' }
+  };
+    const { data } = await axios.get('https://api.thecatapi.com/v1/images/search', config);
+    debugger;
+    breed.value = data[0]
+});
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <TheMain />
+  </main>
+</template>
+
+<script setup>
+import TheMain from '../components/TheMain.vue'
+</script>
